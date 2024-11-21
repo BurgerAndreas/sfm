correct log-likelihood (sign), dtype, plotting

src/sfm/distributions.py
@@ -2,6 +2,7 @@ import math
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import numpy as np
 
 from sklearn.datasets import make_moons
 from torch import Tensor
@@ -127,14 +128,14 @@ class MixtureOfGaussians(CoupledSourceDistribution):
     pis = [0.5, 0.5]  # Equal mixture weights
     """
 
-    def __init__(self, mus: Tensor, sigmas: Tensor, pis: Tensor, data_dim: int = 2, **kwargs):
+    def __init__(self, mus: Tensor, sigmas: Tensor, pis: Tensor, data_dim: int = 2, dtype: torch.dtype = torch.float32, **kwargs):
         # means (components x dimensions)
-        super().__init__(data_dim, **kwargs)
-        mus = torch.as_tensor(mus)
+        super().__init__(data_dim, dtype=dtype, **kwargs)
+        mus = torch.as_tensor(mus, dtype=dtype)
         # std deviations (components x dimensions)
-        sigmas = torch.as_tensor(sigmas)
+        sigmas = torch.as_tensor(sigmas, dtype=dtype)
         # mixture weights (components)
-        pis = torch.as_tensor(pis)
+        pis = torch.as_tensor(pis, dtype=dtype)
         mix = torch.distributions.Categorical(pis)
         self.K = len(mus)  # Number of components
         assert mus.shape == (self.K, data_dim), f"mus has wrong shape {mus.shape}"
@@ -142,7 +143,9 @@ class MixtureOfGaussians(CoupledSourceDistribution):
         assert pis.shape == (self.K,), f"pis has wrong shape {pis.shape}"
         assert all(0 <= pi <= 1 for pi in pis), "all pis must be between 0 and 1"
         assert sum(pis) == 1, "pis must sum to 1"
-        comp = torch.distributions.Independent(torch.distributions.Normal(mus, sigmas), 1)
+        comp = torch.distributions.Independent(
+            torch.distributions.Normal(mus, sigmas), 1
+        )
         self.dist = torch.distributions.MixtureSameFamily(mix, comp)
 
 

src/sfm/plot_cfm_gif.py
@@ -67,9 +67,8 @@ def plot_cfm_gif(args: DictConfig) -> None:
     )
     
     model = MLP(dim=2, time_varying=True)
-    cp = torch.load(os.path.join(args.savedir, args.cpname + ".pth"))
+    cp = torch.load(os.path.join(args.savedir, args.cpname + ".pth"), weights_only=True)
     model.load_state_dict(cp)
-    print(f"model:\n{model}")
     
     sourcedist = get_source_distribution(**args.source)
 
@@ -111,6 +110,7 @@ def plot_cfm_gif(args: DictConfig) -> None:
         iplot = 0
         
         ### log-probability / density plot
+        # sample probability of points on a grid
         if args.doplot[0]:
             cnf = DEFunc(CNF(model))
             nde = NeuralODE(cnf, solver="euler", sensitivity="adjoint")
@@ -133,8 +133,6 @@ def plot_cfm_gif(args: DictConfig) -> None:
                     # log_probs = log_8gaussian_density(gridpoints)
                     log_probs = sourcedist.log_prob(gridpoints)
             assert log_probs.shape == (points_real**2,), f"log_probs.shape: {log_probs.shape}"
-            # print(f"Log-prob of sample: {sourcedist.log_prob(sample).mean().item()}")
-            tqdm.write(f"Log-prob of sample under model: {log_probs.mean().item():0.2f}")
             log_probs = log_probs.reshape(Y.shape)
             ax = axis[iplot]
             ax.pcolormesh(X, Y, torch.exp(log_probs), vmax=1)

src/sfm/plot_inference_sidebyside.py
@@ -61,9 +61,8 @@ def plot_inference_sidebyside(args: DictConfig) -> None:
     )
     
     model = MLP(dim=2, time_varying=True)
-    cp = torch.load(os.path.join(args.savedir, args.cpname + ".pth"))
+    cp = torch.load(os.path.join(args.savedir, args.cpname + ".pth"), weights_only=True)
     model.load_state_dict(cp)
-    print(f"model:\n{model}")
     
     sourcedist = get_source_distribution(**args.source)
 
@@ -75,16 +74,29 @@ def plot_inference_sidebyside(args: DictConfig) -> None:
     os.makedirs(PLOT_DIR_SOURCE, exist_ok=True)
     
     # plot sample
+    set_plotstyle()
     plt.scatter(sample[:, 0], sample[:, 1], s=1, alpha=0.5)
-    plt.tight_layout()
+    plt.tight_layout(pad=0.0)
     fname = f"{PLOT_DIR_SOURCE}/{args['source']['trgt']}_scatter.png"
     plt.savefig(fname, dpi=40)
     print(f"Saved sample to\n {fname}")
     plt.close()
     
     # plot sample as 2d histogram
-    plt.hist2d(sample[:, 0].numpy(), sample[:, 1].numpy(), bins=100, cmap="coolwarm")
-    plt.tight_layout()
+    set_plotstyle()
+    cmap = "coolwarm"
+    # cmap = sns.color_palette("Spectral", as_cmap=True)
+    # plt.hist2d(
+    #     sample[:, 0].numpy(), sample[:, 1].numpy(), bins=100, cmap=cmap
+    # )
+    # sns.kdeplot(
+    #     x=sample[:, 0].numpy(), y=sample[:, 1].numpy(), cmap=cmap, fill=True
+    # )
+    sns.histplot(
+        x=sample[:, 0].numpy(), y=sample[:, 1].numpy(), cmap=cmap, fill=True,
+        bins=100
+    )
+    plt.tight_layout(pad=0.0)
     fname = f"{PLOT_DIR_SOURCE}/{args['source']['trgt']}_hist2d.png"
     plt.savefig(fname, dpi=40)
     print(f"Saved hist2d to\n {fname}")
@@ -97,9 +109,7 @@ def plot_inference_sidebyside(args: DictConfig) -> None:
     # with torch.no_grad():
     # [n_t_gif, n_samples, 2]
     traj = nde.trajectory(sample.to(device), t_span=ts.to(device)).detach().cpu().numpy()
-    
     set_plotstyle()
-    
     n_plots = 1
     for i, t in tqdm(enumerate(ts)):
         fig, axes = plt.subplots(n_plots, n_models, figsize=(6 * n_models, 6 * n_plots))
@@ -131,13 +141,20 @@ def plot_inference_sidebyside(args: DictConfig) -> None:
         # tqdm.write(f"Log-prob of sample under model: {log_probs.mean().item():0.2f}")
         log_probs = log_probs.reshape(Y.shape)
         ax = axis
-        ax.pcolormesh(X, Y, torch.exp(log_probs), vmax=1)
+        # viridis coolwarm BuPu PuRd magma inferno cividis prism ocean
+        cmap = "viridis" 
+        # cmap = sns.color_palette("Spectral", as_cmap=True)
+        ax.pcolormesh(
+            X, Y, torch.exp(log_probs), vmax=1, cmap=cmap,
+            # shading{'flat', 'nearest', 'gouraud', 'auto'}
+        )
         ax.set_xticks([])
         ax.set_yticks([])
         ax.set_xlim(-w, w)
         ax.set_ylim(-w, w)
         # ax.set_title(f"{args.runname}", fontsize=30)
-        plt.tight_layout(pad=0.0)
+        # can't get rid of white border, so pad a bit to make it look cleaner
+        plt.tight_layout(pad=0.1) 
         plt.savefig(f"{tempdir}/{tplotname}_{t:0.2f}.png", dpi=40)
         plt.close()
 
@@ -152,6 +169,8 @@ def plot_inference_sidebyside(args: DictConfig) -> None:
     # plot each image
     for i, (ax, img) in enumerate(zip(axes, images)):
         ax.imshow(img)
+        # remove border between plots
+        ax.axis("off")
         ax.set_xticks([])
         ax.set_yticks([]) 
         ax.set_title(f"T={ts[i]:0.2f}")